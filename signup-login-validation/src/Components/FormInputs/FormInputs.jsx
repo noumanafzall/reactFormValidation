896098ab3ce0c61c74a743f884d8613edd0bef33
noumanafzall <noumanafzal.business@gmail.com>
2023-12-03 14:35:01 +0500

    Validation

--- signup-login-validation/src/Components/FormInputs/FormInputs.jsx
+++ signup-login-validation/src/Components/FormInputs/FormInputs.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FormInputs.css'
+
 
 export const FormInputs = (props) => {
 
-    const {label, errorMessage, onChange, id, ...inputProps} = props
+  const [focused,setFocused] = useState(false)
+  const {label, errorMessage, onChange, id, ...inputProps} = props
   
-    return (
-      <div className='form-inputs'>
-          <label>{label}</label>
-          <input {...inputProps} onChange={onChange}/>
-          <span>{errorMessage}</span>
-      </div>
-    )
+  const handleFocus = (e) => {
+    setFocused(true)
   }
+  return (
+    <div className='form-inputs'>
+        <label>{label}</label>
+        <input 
+          {...inputProps} onChange={onChange}
+          onBlur={handleFocus} 
+          onFocus={ () => 
+              inputProps.name === "confirmPassword" && setFocused(true)
+            }
+          focused={focused.toString()}
+        />
+        <span>{errorMessage}</span>
+    </div>
+  )
+}
 
 export default FormInputs
